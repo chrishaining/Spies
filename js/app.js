2b7bf572163abdfcf47690a27072bf1722c5606d
Chris Haining <chrishaining@yahoo.co.uk>
2019-09-07 15:50:12 +0100

    plays around with css

--- js/app.js
+++ js/app.js
@@ -28,9 +28,9 @@ const createSpy = function (form) {
   government.textContent = `Government: ${form.government.value}`;
   spy.appendChild(government);
 
-  const defectability = document.createElement('h3');
-  defectability.textContent = `Likelihood of defecting: ${form.defectability.value}`;
-  spy.appendChild(defectability);
+  const skill = document.createElement('h3');
+  skill.textContent = `Skill at killing: ${form.skill.value}`;
+  spy.appendChild(skill);
 
   const trustworthiness = document.createElement('h3');
   trustworthiness.textContent = `Trustworthiness: ${form.trustworthiness.value}`;
@@ -55,3 +55,12 @@ const handleDeleteAllClick = function (event) {
   const listOfSpies = document.querySelector('#spy-list');
   listOfSpies.innerHTML = '';
 }
+
+//potential function to pit two randomly selected spies against each other.
+//1. randomly selects two spies (this implies there may need to be an array of spies, unless we can use random on listOfSpies)
+//2. if spy1 === spy2, the function should reselect spy 2. if spy1 and spy2 have the same government, then the function should also reselect spy2 (or - what about having this as a joker, see step 5.)
+//3. compare spy1 and spy2's persuasiveness and/or likelihoodOfDefecting. result is either that one spy defects or there is a draw. if there is a defection, the function ends and returns the result
+//4. if step 3 results in a draw, compare spy1 and spy2's killing skill. the result is either that one spy kills the other, or - if both have equal killing skill - they both get killed.
+//5. possible additional element is to have a joker. this could bring down the government, or start a killing spree to wipe out the entire list of spies (delete all).
+
+//other possible function is to have a last spy standing game. it would be like the other function, but wouldn't have a joker and would keep going until only one spy was left. to ensure the game runs smoothly, it would need to deal with spies of the same government (it would ignore the defection). the game would keep looping while array.length > 1. (or until array.length === 1)
